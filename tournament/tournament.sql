27197ca628ff8ca722e474f48eae63c95d5ea060
Edwin Marte <kdgpro15@gmail.com>
2017-06-23 19:10:54 -0400

    Polishing Files and Finishing Project

--- tournament/tournament.sql
+++ tournament/tournament.sql
@@ -19,6 +19,7 @@ CREATE TABLE matches (
     loser   integer REFERENCES players(id),
 );
 
+-- Creating View for Player Standings
 CREATE OR REPLACE VIEW player_standings AS
   SELECT
     players.id,
@@ -30,4 +31,31 @@ CREATE OR REPLACE VIEW player_standings AS
   FROM
     (players LEFT JOIN matches ON winner = players.id)
   GROUP BY
-    players.id;
+    players.id
+  ORDER BY
+    wins DESC;
+
+-- Creating View for Even Player Standings
+CREATE OR REPLACE VIEW even_player_standings AS
+  SELECT id, name, rn
+  FROM
+    (SELECT id, name, wins, ROW_NUMBER() OVER() rn FROM player_standings) as even
+  WHERE MOD(rn, 2) = 0;
+
+-- Creating View for Uneven Player Standings
+CREATE OR REPLACE VIEW uneven_player_standings AS
+  SELECT id, name, rn
+  FROM
+    (SELECT id, name, wins, ROW_NUMBER() OVER() rn FROM player_standings) as uneven
+  WHERE MOD(rn, 2) <> 0;
+
+-- Creating View for Players Pairs Standings
+CREATE OR REPLACE VIEW pl_pairs AS
+  SELECT
+    uneven.id AS id1,
+    uneven.name AS name1,
+    even.id AS id2,
+    even.name AS name2
+  FROM
+    even_player_standings AS even JOIN uneven_player_standings AS uneven
+  ON uneven.rn = (even.rn-1);
